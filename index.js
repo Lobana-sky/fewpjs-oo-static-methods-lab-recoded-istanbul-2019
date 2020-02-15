222dade1b35226be66b59943504630aa5ddccbb4
lobana alarabi <loubanaeng@gmail.com>
2020-02-15 00:37:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ class Formatter {
      if(e=="the"||e=="an"||e=="but"||e=="of"||e=="and"||e=="for"||e=="by"||e=="from"||e=="at"){
        return e;
      }
+     else if(e=="a")
      else {
        return this.capitalize(e);
      } 
