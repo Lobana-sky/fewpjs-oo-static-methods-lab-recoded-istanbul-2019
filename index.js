1c85707e25511a6d4c13f4c713f0e2d43d36254c
lobana alarabi <loubanaeng@gmail.com>
2020-02-15 00:35:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase()+string.slice(1);
+    
+  }
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+  static titleize(string){
+    let x=string.split(" ");
+ return  x.map(e=>{
+     if(e=="the"||e=="a"||e=="an"||e=="but"||e=="of"||e=="and"||e=="for"||e=="by"||e=="from"||e=="at"){
+       return e;
+     }
+     else {
+       return this.capitalize(e);
+     } 
+   }).join(" ");
+  }
 }
