fe67eb99e4351e449b987e1543af24e0029715e6
lobana alarabi <loubanaeng@gmail.com>
2020-02-15 00:38:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ class Formatter {
      if(e=="the"||e=="an"||e=="but"||e=="of"||e=="and"||e=="for"||e=="by"||e=="from"||e=="at"){
        return e;
      }
-     else if(e=="a")
+     else if(e=="a"){
+       
+     }
      else {
        return this.capitalize(e);
      } 
